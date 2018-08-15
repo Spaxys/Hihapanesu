32d61ae3b7e07ebea0212b475a7c6b3d7dfac945
Daniel Webiörn <spaxys@live.se>
2018-08-15 19:56:29 +0200

    Major Commit: Basic web gui now working!

--- Hihapanesu.Web/Scripts/TranslateScripts.js
+++ Hihapanesu.Web/Scripts/TranslateScripts.js
@@ -1,5 +1,4 @@
 ﻿function HihapanesuTranslate(input) {
-    debugger;
     var dataObject = {
         Text: input,
         IsTest: false
@@ -17,83 +16,19 @@
         contentType: "application/json; charset=utf-8",
         //dataType: "json",
         success: function (data) {
-            debugger;
-            //resultData = data.responseText
         },
         failure: function (errmsg) {
-            debugger;
             alert(errMsg);
         }
     });
     var responseText = jqXHR.responseText;
-    console.log(responseText);
+    var responseToString = JSON.parse(responseText);
+    //console.log(responseText);
 
-    return responseText;
-
-    //$.ajax({
-    //    type: "POST",
-    //    method: "POST",
-    //    url: "api/Translate/TranscribeAndGenerate",
-    //    data: dataObject,
-    //    contentType: "application/json; charset=utf-8",
-    //    success: function (data) {
-    //        debugger;
-    //        resultData = data
-    //    },
-    //    failure: function (errmsg) {
-    //        debugger;
-    //        alert(errMsg);
-    //    }
-    //});
-
-    //$.ajax({
-    //    type: "POST",
-    //    method: "POST",
-    //    url: "api/Translate/TranscribeAndGenerate",
-    //    data: dataObject,
-    //    contentType: "application/json; charset=utf-8",
-    //    success: function (data) {
-    //        debugger;
-    //        resultData = data
-    //    },
-    //    failure: function (errmsg) {
-    //        debugger;
-    //        alert(errMsg);
-    //    }
-    //});
-
-    //$.ajax({
-    //    method: "POST",
-    //    url: url,
-    //    data: dataObject,
-    //    success: function (data) {
-    //        console.log(data);
-    //        debugger;
-    //        resultData = data;
-    //    },
-    //    failure: function (errmsg) {
-    //        debugger;
-    //        alert(errMsg);
-    //    }
-    //});
-
-    //jQuery.post('api/Translate/TranscribeAndGenerate', dataJson, function (data) {
-    //    resultData = data
-    //}, 'json');
-
-
-    //Vanilla Js ajax version
-    //var xhttp = new XMLHttpRequest();
-
-
-    //return resultData;
-
-    //var boldResult = "<b>" + input + "</b>";
-    //return boldResult;
+    return responseToString;
 }
 
 function SetHihapanesuValue(elementId, html) {
-    debugger;
     var resultElem = document.getElementById(elementId);
     resultElem.innerHTML = html;
 }
