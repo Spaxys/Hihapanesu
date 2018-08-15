8334eb8b88e3aeab0bca90355dbf5b4d9ed088b2
Daniel Webiörn <spaxys@live.se>
2018-08-15 18:56:12 +0200

    WIP: Now successfully sending POST request and receiving a response. TODO: Fix html parsing of the SVG result.

--- Hihapanesu.Web/Controllers/TranslateController.cs
+++ Hihapanesu.Web/Controllers/TranslateController.cs
@@ -3,9 +3,11 @@ using Hihapanesu.Web.Services;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Results;
 
@@ -28,16 +30,60 @@ namespace Hihapanesu.Web.Controllers
 
         [Route("TranscribeAndGenerate")]
         [HttpPost]
-        public HttpResponseMessage TranscribeAndGenerate(TranscribeAndGenerateModel model)
+        public async Task<HttpResponseMessage> TranscribeAndGenerate(TranscribeAndGenerateModel model)
         {
             var service = new ElvishTranslationService("elvishSymbols.svg");
             var result = service.TranscribeAndGenerate(model.Text, model.IsTest);
-            //var 
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            response.Content.
-            using (var device = new Kean.IO.Chara
-            var resultString = result.ToString();
-            return 
+            var bigData = new List<byte>();
+            var data = new byte[1024 * 1024];
+            //byte[] data;
+            var stringContent = "";
+            var cancellationToken = new System.Threading.CancellationToken();
+            using (var stream = new System.IO.MemoryStream(data))
+            {
+                //using (var device = Kean.IO.ByteDevice.Open(stream))
+                using (var device = Kean.IO.CharacterDevice.Open(stream))
+                {
+                    //var locatorDevice = 
+                    using (var writer = Kean.IO.CharacterWriter.Open(device))
+                    {
+
+                        result.Save(writer);
+                        //stringContent = result.ToString();
+
+                        //var reader = new StreamReader(stream);
+                        //stringContent = reader.ReadToEnd();
+                        //    var buffer = new byte[stream.Length];
+                        //stream.Read(buffer, 0, (int)stream.Length);
+
+                        var lastChars = "";
+                        var endOfFile = "</xml>";
+                        var nullCount = 0;
+                        foreach(var c in data)
+                        {
+                            if (lastChars.Length == 4)
+                            {
+                                lastChars.Remove(lastChars.Length - 1, 1);
+                                lastChars = c + lastChars;
+                            }
+                            if (c == '\0')
+                                break;
+                            if (lastChars == endOfFile)
+                                break;
+                            stringContent += (char)c;
+                        }
+                        stringContent += endOfFile;
+                    }
+                }
+            }
+            //HttpContent bytesContent = new ByteArrayContent(byteData);
+            //using (var formData = new MultipartFormDataContent())
+            return Request.CreateResponse(HttpStatusCode.OK, stringContent);
+            //    var response = this.ResponseMessage(new HttpResponseMessage(HttpStatusCode.OK));
+            //response.Response.
+            //    using (var device = new Kean.IO.Chara
+            //var resultString = result.ToString();
+            //return 
         }
     }
 }
---
+++ Hihapanesu.Web/Scripts/TranslateScripts.js
@@ -0,0 +1,99 @@
+﻿function HihapanesuTranslate(input) {
+    debugger;
+    var dataObject = {
+        Text: input,
+        IsTest: false
+    };
+
+    var dataJson = JSON.stringify(dataObject);
+    var resultData;
+
+    var url = "../api/Translate/TranscribeAndGenerate";
+    var jqXHR = $.ajax({
+        type: "POST",
+        url: url,
+        data: dataJson,
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        //dataType: "json",
+        success: function (data) {
+            debugger;
+            //resultData = data.responseText
+        },
+        failure: function (errmsg) {
+            debugger;
+            alert(errMsg);
+        }
+    });
+    var responseText = jqXHR.responseText;
+    console.log(responseText);
+
+    return responseText;
+
+    //$.ajax({
+    //    type: "POST",
+    //    method: "POST",
+    //    url: "api/Translate/TranscribeAndGenerate",
+    //    data: dataObject,
+    //    contentType: "application/json; charset=utf-8",
+    //    success: function (data) {
+    //        debugger;
+    //        resultData = data
+    //    },
+    //    failure: function (errmsg) {
+    //        debugger;
+    //        alert(errMsg);
+    //    }
+    //});
+
+    //$.ajax({
+    //    type: "POST",
+    //    method: "POST",
+    //    url: "api/Translate/TranscribeAndGenerate",
+    //    data: dataObject,
+    //    contentType: "application/json; charset=utf-8",
+    //    success: function (data) {
+    //        debugger;
+    //        resultData = data
+    //    },
+    //    failure: function (errmsg) {
+    //        debugger;
+    //        alert(errMsg);
+    //    }
+    //});
+
+    //$.ajax({
+    //    method: "POST",
+    //    url: url,
+    //    data: dataObject,
+    //    success: function (data) {
+    //        console.log(data);
+    //        debugger;
+    //        resultData = data;
+    //    },
+    //    failure: function (errmsg) {
+    //        debugger;
+    //        alert(errMsg);
+    //    }
+    //});
+
+    //jQuery.post('api/Translate/TranscribeAndGenerate', dataJson, function (data) {
+    //    resultData = data
+    //}, 'json');
+
+
+    //Vanilla Js ajax version
+    //var xhttp = new XMLHttpRequest();
+
+
+    //return resultData;
+
+    //var boldResult = "<b>" + input + "</b>";
+    //return boldResult;
+}
+
+function SetHihapanesuValue(elementId, html) {
+    debugger;
+    var resultElem = document.getElementById(elementId);
+    resultElem.innerHTML = html;
+}
