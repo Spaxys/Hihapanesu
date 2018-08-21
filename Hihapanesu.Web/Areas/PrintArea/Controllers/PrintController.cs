0e4b434f51f604ab32408f4c441eb087f51fc90b
Daniel Webiörn <spaxys@live.se>
2018-08-21 06:52:12 +0200

    WIP Getting the post method to open a new tab with the svg data. Currently not working yet.

---
+++ Hihapanesu.Web/Areas/PrintArea/Controllers/PrintController.cs
@@ -0,0 +1,30 @@
+﻿using Hihapanesu.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Hihapanesu.Web.Areas.PrintArea.Controllers
+{
+    public class PrintController : Controller
+    {
+        // GET: PrintArea/Print
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Print(string data)
+        {
+            var model = new PrintVM { Data = data };
+            return View(model);
+        }
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Print([FromBody]PrintVM model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Hihapanesu.Web/Areas/PrintArea/PrintAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Hihapanesu.Web.Areas.PrintArea
+{
+    public class PrintAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PrintArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PrintArea_default",
+                "PrintArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
